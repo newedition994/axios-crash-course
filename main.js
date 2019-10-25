58b695055a01f372fe5b2d887374b026d41b4cea
Breyanna Vass <breyannavass@gmail.com>
2019-10-25 13:03:28 -0400

    code for handling errors

--- main.js
+++ main.js
@@ -100,7 +100,26 @@ function transformResponse() {
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log("Error Handling");
+  axios
+    .get("https://jsonplaceholder.typicode.com/todoss")
+    .then(res => showOutput(res))
+    .catch(err => {
+      if (err.response) {
+        // Server responded status other than 200 range
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+
+        if (err.response.status === 404) {
+          alert("Error: Page Not Found");
+        }
+      } else if (err.request) {
+        // Request was made but no response
+        console.error(err.request);
+      } else {
+        console.error(err.message);
+      }
+    });
 }
 
 // CANCEL TOKEN
