532d09f41c43d4c97e7ffadf3b1097bd86ac656c
Breyanna Vass <breyannavass@gmail.com>
2019-10-25 12:02:20 -0400

    defining the simultaneous data gathering

--- main.js
+++ main.js
@@ -47,7 +47,13 @@ function removeTodo() {
 
 // SIMULTANEOUS DATA
 function getData() {
-  console.log("Simultaneous Request");
+  axios
+    .all([
+      axios.get("https://jsonplaceholder.typicode.com/todos?_limit=5"),
+      axios.get("https://jsonplaceholder.typicode.com/posts?_limit=5")
+    ])
+    .then(axios.spread((todos, posts) => showOutput(posts)))
+    .catch(err => console.log(err));
 }
 
 // CUSTOM HEADERS
