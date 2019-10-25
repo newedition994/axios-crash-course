56d57904017335378f81f6b71cf53769c68f86e5
Breyanna Vass <breyannavass@gmail.com>
2019-10-25 12:28:49 -0400

    adding intercepting requests and responses'

--- main.js
+++ main.js
@@ -77,7 +77,19 @@ function cancelToken() {
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
-
+axios.interceptors.request.use(
+  config => {
+    console.log(
+      `${config.method.toUpperCase()} request sent to ${
+        config.url
+      } at ${new Date().getTime()}`
+    );
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 // AXIOS INSTANCES
 
 // Show output in browser
