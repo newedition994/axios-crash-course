1a9c985e202906badaaa53ee7cee88320d086824
Breyanna Vass <breyannavass@gmail.com>
2019-10-25 11:38:48 -0400

    showing data on the website'

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // GET REQUEST
 function getTodos() {
-  console.log("GET Request");
+  axios({
+    method: "get",
+    url: "https://jsonplaceholder.typicode.com/todos"
+  })
+    .then(res => showOutput(res))
+    .catch(err => console.log(err));
 }
 
 // POST REQUEST
